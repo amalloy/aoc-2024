3e9a55d48245d33f18f45c101003ccae01534a63
Alan Malloy <alan@malloys.org>
2024-12-02 07:35:19 -0800

    Day 2, part 1

--- day02/src/Main.hs
+++ day02/src/Main.hs
@@ -1,21 +1,24 @@
-{-# LANGUAGE ApplicativeDo #-}
-{-# LANGUAGE LambdaCase #-}
-
 module Main where
 
 import Control.Arrow ((&&&))
 
-type Input = [String]
+type Level = Int
+type Record = [Level]
+type Input = [Record]
 
-part1 :: Input -> ()
-part1 = const ()
+part1 :: Input -> Int
+part1 = length . filter safe
+  where safe = liftA2 (&&) sameSign (all small) . deltas
+        sameSign [] = True
+        sameSign (x:xs) = all (== signum x) $ map signum xs
+        deltas = zipWith (-) <*> tail
+        small = liftA2 (&&) (>= 1) (<= 3) . abs
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = map (map read . words) . lines
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
-
