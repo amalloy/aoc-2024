00cfed7c4e5449ceca5b1d7a6450b6e5928a192a
Alan Malloy <alan@malloys.org>
2024-12-03 23:17:56 -0800

    Day 4, part 2

--- day04/src/Main.hs
+++ day04/src/Main.hs
@@ -1,8 +1,9 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Control.Monad (guard)
 
-import Data.List (tails, transpose, isPrefixOf)
+import Data.List (tails, transpose, isPrefixOf, sort)
 
 type Input = [String]
 
@@ -22,8 +23,18 @@ searchGrids xs = do
 part1 :: Input -> Int
 part1 = length . filter ("XMAS" `isPrefixOf`) . searchGrids
 
-part2 :: Input -> ()
-part2 = const ()
+subMatricesOfSize :: Int -> [[a]] -> [[[a]]]
+subMatricesOfSize n xs = do
+  grid <- tails xs
+  let window = take n grid
+  guard $ length window == n
+  let lens = map (map (take n) . tails) window
+  transpose lens
+
+part2 :: Input -> Int
+part2 = length . filter isXmas . subMatricesOfSize 3
+  where isXmas [[a, _, b], [_, 'A', _], [c, _, d]] = sort [a, d] == "MS" && sort [b, c] == "MS"
+        isXmas _ = False
 
 prepare :: String -> Input
 prepare = lines
