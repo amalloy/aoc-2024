0cfd4ff963d15f7e5c2a96a1155b51b786e6e918
Alan Malloy <alan@malloys.org>
2024-12-03 22:52:08 -0800

    Day 4, part 1

--- day04/src/Main.hs
+++ day04/src/Main.hs
@@ -2,10 +2,25 @@ module Main where
 
 import Control.Arrow ((&&&))
 
+import Data.List (tails, transpose, isPrefixOf)
+
 type Input = [String]
 
-part1 :: Input -> ()
-part1 = const ()
+transpositions :: [[a]] -> [[[a]]]
+transpositions = take 4 . iterate (reverse . transpose)
+
+diagonals :: [[a]] -> [[a]]
+diagonals [] = []
+diagonals arr@(_:more) = transpose (zipWith drop [0..] arr) ++ diagonals more
+
+searchGrids :: [[a]] -> [[a]]
+searchGrids xs = do
+  grid <- transpositions xs
+  orientation <- [tails =<< grid, diagonals grid]
+  orientation
+
+part1 :: Input -> Int
+part1 = length . filter ("XMAS" `isPrefixOf`) . searchGrids
 
 part2 :: Input -> ()
 part2 = const ()
