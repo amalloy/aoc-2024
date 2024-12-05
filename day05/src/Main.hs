02ff208cc8a101826c21bccd329168973d13dd85
Alan Malloy <alan@malloys.org>
2024-12-04 21:58:58 -0800

    Day 5, parser

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -1,17 +1,39 @@
 module Main where
 
+import Control.Applicative (Alternative, many)
 import Control.Arrow ((&&&))
 
-type Input = [String]
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 
-part1 :: Input -> ()
-part1 = const ()
+import Text.Regex.Applicative (RE, match, sym)
+import Text.Regex.Applicative.Common (decimal)
+
+type Page = Int
+data Dependency = Dependency {before, after :: Page} deriving Show
+type RuleOrdering = M.Map Page (S.Set Page)
+type Update = [Page]
+
+data Input = Input RuleOrdering [Update] deriving Show
+
+sepBy1 :: Alternative f => f a -> f b -> f [a]
+sepBy1 p sep = (:) <$> p <*> many (sep *> p)
+
+input :: RE Char Input
+input = Input <$> (ruleOrdering <* sym '\n') <*> many update
+  where ruleOrdering = (M.fromListWith (<>) . map toKv) <$> many dependency
+        dependency = Dependency <$> (decimal <* sym '|') <*> (decimal <* sym '\n')
+        toKv (Dependency before after) = (after, S.singleton before)
+        update = (decimal `sepBy1` sym ',') <* sym '\n'
+
+part1 :: Input -> Input
+part1 = id
 
 part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = maybe (error "no parse") id . match input
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
