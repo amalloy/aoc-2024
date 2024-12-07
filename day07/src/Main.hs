30b848b260a51fb2be755b1f65e01bf46195e7b0
Alan Malloy <alan@malloys.org>
2024-12-07 11:48:02 -0800

    Day 7: Use NonEmpty for equation components (@Melvar)

--- day07/src/Main.hs
+++ day07/src/Main.hs
@@ -2,10 +2,12 @@ module Main where
 
 import Control.Arrow ((&&&))
 
+import Data.List.NonEmpty (NonEmpty(..), some1)
+
 import Text.Regex.Applicative (match, sym, many)
 import Text.Regex.Applicative.Common (decimal)
 
-data Equation a = Equation a [a] deriving Show
+data Equation a = Equation a (NonEmpty a) deriving Show
 data Solution a = Solution a a [(Operator, a)] deriving Show
 data Operator = Plus | Times | Cat deriving (Show, Enum, Bounded)
 type Input = [Equation Int]
@@ -16,7 +18,7 @@ apply Times = (*)
 apply Cat = \a b -> read (show a <> show b)
 
 solutions :: (Read a, Show a, Num a, Ord a) => [Operator] -> Equation a -> [Solution a]
-solutions ops (Equation goal (num:nums)) = Solution goal num <$> go num nums
+solutions ops (Equation goal (num :| nums)) = Solution goal num <$> go num nums
   where go total atoms = case (compare total goal, atoms) of
           (GT, _) -> []
           (LT, []) -> []
@@ -38,7 +40,7 @@ part2 = solve [Times, Plus, Cat]
 prepare :: String -> Input
 prepare = maybe (error "no parse") id . match input
   where input = many (equation <* sym '\n')
-        equation = Equation <$> (decimal <* sym ':') <*> (many (sym ' ' *> decimal))
+        equation = Equation <$> (decimal <* sym ':') <*> (some1 (sym ' ' *> decimal))
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
