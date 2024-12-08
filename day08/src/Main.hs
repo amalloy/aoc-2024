e0e81b197b6ccad5535a830818868f564a4fb6e8
Alan Malloy <alan@malloys.org>
2024-12-07 21:44:34 -0800

    Day 8, parser & types

--- day08/src/Main.hs
+++ day08/src/Main.hs
@@ -2,7 +2,17 @@ module Main where
 
 import Control.Arrow ((&&&))
 
-type Input = [String]
+import Data.Char (isAlphaNum)
+import Data.Ix (inRange)
+import Data.Map.Strict qualified as M
+import Data.Set qualified as S
+
+import Linear.V2 (V2(..))
+
+type Coord = V2 Int
+type Frequency = Char
+
+data Input = Input (Coord, Coord) (M.Map Frequency (S.Set Coord)) deriving Show
 
 part1 :: Input -> ()
 part1 = const ()
@@ -11,7 +21,16 @@ part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare text = Input bounds antennas
+  where bounds = (V2 1 1, V2 (length indexed) (length $ head indexed))
+        antennas = M.fromListWith S.union $ do
+          (pos, c) <- concat indexed
+          case c of
+            '.' -> []
+            x | isAlphaNum x -> [(x, S.singleton pos)]
+              | otherwise -> error $ "Unexpected character " <> [x]
+        indexed :: [[(Coord, Char)]]
+        indexed = zipWith (\y r -> zipWith (\x c -> (V2 y x, c)) [1..] r) [1..] (lines text)
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
