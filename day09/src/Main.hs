e307fe201c03788a42eb7ceca3fb71a8df95bbd1
Alan Malloy <alan@malloys.org>
2024-12-08 21:42:01 -0800

    Day 9, types & parser

--- day09/src/Main.hs
+++ day09/src/Main.hs
@@ -1,8 +1,13 @@
 module Main where
 
 import Control.Arrow ((&&&))
+import Data.Array (assocs)
+import Data.Array.ST (newListArray, runSTArray, readArray, writeArray)
+import Data.Char (digitToInt, isDigit)
 
-type Input = [String]
+data Block = Free | File Int deriving Show
+
+type Input = [Block]
 
 part1 :: Input -> ()
 part1 = const ()
@@ -11,7 +16,11 @@ part2 :: Input -> ()
 part2 = const ()
 
 prepare :: String -> Input
-prepare = lines
+prepare = file 0 . map digitToInt . filter isDigit
+  where file _ [] = []
+        file n (size:rest) = replicate size (File n) <> empty n rest
+        empty _ [] = []
+        empty n (size:rest) = replicate size Free <> file (n + 1) rest
 
 main :: IO ()
 main = readFile "input.txt" >>= print . (part1 &&& part2) . prepare
