0dc5a98c3e6f5802535ac06e0abd367fd2625a1c
Alan Malloy <alan@malloys.org>
2024-12-10 22:28:33 -0800

    Day 11: Simplify by inlining `split`

--- day11/src/Main.hs
+++ day11/src/Main.hs
@@ -1,7 +1,6 @@
 module Main where
 
 import Control.Arrow ((&&&))
-import Data.Bifunctor (bimap)
 
 import qualified Data.IntMap as M
 
@@ -13,17 +12,13 @@ type Input = StoneCounts
 freqs :: [Label] -> StoneCounts
 freqs = M.fromListWith (+) . map (, 1)
 
-split :: Label -> Maybe (Label, Label)
-split n | even len = Just $ bimap read read (splitAt (len `div` 2) s)
-        | otherwise = Nothing
-  where s = show n
-        len = length s
-
 step :: Label -> StoneCounts
 step 0 = M.singleton 1 1
-step n = case split n of
-  Just (a, b) -> freqs [a, b]
-  Nothing -> M.singleton (n * 2024) 1
+step n | even len = let (a, b) = splitAt (len `div` 2) s
+                    in freqs [read a, read b]
+       | otherwise = M.singleton (n * 2024) 1
+  where s = show n
+        len = length s
 
 blink :: StoneCounts -> StoneCounts
 blink m = M.unionsWith (+) $ do
